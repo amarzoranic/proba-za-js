aa001c4899a63799c96db11456cdb65fa118fc81
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-22 00:56:27 +0200

    promena

--- 2.zadatak-js/app.js
+++ 2.zadatak-js/app.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 broj = prompt("Unesi broj");
 if (broj >= 0) {
     console.log("Uneli ste pozitivan broj");
 }
 else {
     console.log("Uneli ste negativan broj");
-=======
-broj = prompt("Unesi broj");
-if (broj >= 0) {
-    console.log("Uneli ste pozitivan broj");
-}
-else {
-    console.log("Uneli ste negativan broj");
-}
