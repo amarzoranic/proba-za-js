02f68f390063352d3b9c0c621cf3e7c8fef251dc
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-25 17:02:41 +0200

    zadatak

---
+++ 7.zadatak-js/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+niz = [1,2,3,4,5,6,7,8,9,10]
+for (i=1; i <= niz.length; i++){
+   console.log(i);
+} niz2 = Number(prompt("Unesi niz sa parnim brojevima"))
+for (i=1; i <= niz.length; i++){
+    if (i % 2=== 0){
+        console.log(i);
+    }
+}
+
+
+
+
+
