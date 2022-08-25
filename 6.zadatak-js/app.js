7c444a5d790d9ed2bcf623f3a5a9d2caa9f92ca6
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-25 17:07:33 +0200

    promena

--- 6.zadatak-js/app.js
+++ 6.zadatak-js/app.js
@@ -3,13 +3,7 @@ if (slovo === "a" || slovo === "e" || slovo === "i" || slovo === "o" || slovo ==
     alert("Samoglasnik je unet");
 }  if (Number(slovo)) {
     prompt("Uneli ste broj. Unesite samoglasnik!")
-} while (slovo === "q" || slovo === "w" || slovo === "r" || slovo === "t" || slovo === "y" || slovo === "p" || slovo === "s"||slovo === "d" || slovo === "f" || slovo === "g" || slovo === "h" || slovo === "j " || slovo === "k"|| slovo === "l" || slovo === ";" || slovo === "z" || slovo === "x" || slovo === "c" || slovo === "v" || slovo === "b" || slovo === "n" || slovo === "m"){
-        prompt("Uneli ste suglasnik. Unesite samoglasnik!");
-        
-        if (slovo === "a" || slovo === "e" || slovo === "i" || slovo === "o" || slovo === "u") {
-            alert("Samoglasnik je unet");
-            break;
-       }
+}
        
        
 
