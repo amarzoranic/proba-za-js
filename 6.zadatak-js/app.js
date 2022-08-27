6ad225d190fffc9d8aa06bd30687c71a93d2861c
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-27 14:18:16 +0200

    n

--- 6.zadatak-js/app.js
+++ 6.zadatak-js/app.js
@@ -1,9 +1,25 @@
-slovo = prompt("Unesite samoglasnik");
-if (slovo === "a" || slovo === "e" || slovo === "i" || slovo === "o" || slovo === "u") {
-    alert("Samoglasnik je unet");
-}  if (Number(slovo)) {
-    prompt("Uneli ste broj. Unesite samoglasnik!")
-}
-       
-       
+// // slovo = prompt("Unesite samoglasnik");
+// // if (slovo === "a" || slovo === "e" || slovo === "i" || slovo === "o" || slovo === "u") {
+// //     alert("Samoglasnik je unet");
+// // }  if (Number(slovo)) {
+// //     prompt("Uneli ste broj. Unesite samoglasnik!")
+// // }
 
+
+
+// array.array.forEach((element) => {
+//     console.log(el + 10);
+// });
+// console.log(object);
+
+
+arr = [
+    [1, 2, 3],
+    [4, 5, 6, 7, 9],
+    [7]
+];
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j], i, j);
+    }
+}
