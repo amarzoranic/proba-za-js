49341e06cf07af9f2c07163260ac1576f98eae1f
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-28 21:35:34 +0200

    zadatak

---
+++ 9.zadatak-js/app.js
@@ -0,0 +1,14 @@
+
+
+unos = Number(prompt("Unestie broj"));
+lastN=0
+for(i =1; i <= unos; i++) {
+    unos1 = ""
+    for(j=0; j < i;j++) {
+        lastN++
+unos1 += lastN;
+    }
+    console.log(unos1);
+}
+
+
