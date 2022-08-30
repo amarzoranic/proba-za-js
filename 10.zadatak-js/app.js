9570ebce2465dd210e4c3ceafe55a4052beeec03
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-30 15:35:17 +0200

    proba

---
+++ 10.zadatak-js/app.js
@@ -0,0 +1,8 @@
+var m = [[25,34], [18,51], [92,44]];  // matrica dimenzije 3x2
+
+  // u konzoli ispisuje jedan po jedan element matrice
+  for (var i=0; i<3; i++) {
+    for (var j=0; j<2; j++) {
+      console.log( m[i][j] );
+    }
+  }
