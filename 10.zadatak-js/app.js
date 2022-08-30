b95d3cfdd4e36b7c427e5f0c5758c9946c34e1c7
Amar Zoranic <amar.zoranic@gmail.com>
2022-08-30 17:18:06 +0200

    promena

--- 10.zadatak-js/app.js
+++ 10.zadatak-js/app.js
@@ -1,8 +1,18 @@
-var m = [[25,34], [18,51], [92,44]];  // matrica dimenzije 3x2
+// var m = [[25,34], [18,51], [92,44]];  // matrica dimenzije 3x2
 
-  // u konzoli ispisuje jedan po jedan element matrice
-  for (var i=0; i<3; i++) {
-    for (var j=0; j<2; j++) {
-      console.log( m[i][j] );
-    }
-  }
+//   // u konzoli ispisuje jedan po jedan element matrice
+//   for (var i=0; i<3; i++) {
+//     for (var j=0; j<2; j++) {
+//       console.log( m[i][j] );
+//     }
+//   }
+unos = Number(prompt("Unesite broj"));
+lastN = 0;
+for (i=1; i <= unos; i++) {
+str = ""
+    for( j=0; j <= i; i++) {
+    lastN++;
+    str += lastN
+}
+console.log(str);
+}
