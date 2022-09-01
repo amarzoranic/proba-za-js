06c2c06a54d0ae1369207d6c8863715bdb7f1a7a
Amar Zoranic <amar.zoranic@gmail.com>
2022-09-02 00:44:56 +0200

    promena

--- 10.zadatak-js/app.js
+++ 10.zadatak-js/app.js
@@ -39,8 +39,40 @@
 
 // }
 
-const number = [2,4,6,8,10]
-const kvadrati = number.map((el) => {
-    return el* el;;
+// const number = [2,4,6,8,10]
+// const kvadrati = number.map((el) => { //map ima tri argument 1. element 2. index 3. array
+//     return el* el;;
+// })
+// console.log(kvadrati);
+
+// const number = [2,4,6,8,10]
+// const koreni = number.map((el) => { //map ima tri argument 1. element 2. index 3. array
+//     return Math.sqrt(el);
+// })
+// console.log(koreni);
+
+// const numbers = [1,2,3,4,5,6,7,8]
+
+// const filtriraniNiz = numbers.filter (el => { //filter da se otkloni ili ocisti
+//     return el % 2 === 0 // ili return el % 2 !=== 0 da nisu parni
+// })
+// console.log(filtriraniNiz);
+
+//niz da je novi gde su pomnozeni sa tri nakot toga da se kvadrira rezultat nakon toga da se filtrira svi brojevi koji su veci od 15
+
+const numbers = [7, 10, 12,14, 4]
+
+const mnozenje = numbers.map ((el) => { 
+    return el *3;
 })
-console.log(kvadrati);
+console.log(mnozenje);
+
+kvadratMnozenja = mnozenje.map ((el) => {
+    return el * el
+})
+console.log(kvadratMnozenja);
+
+filtriranje = kvadratMnozenja.filter ((el) => {
+    return el > 50
+})
+console.log(filtriranje);
