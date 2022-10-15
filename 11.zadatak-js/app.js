ecee5a89644598e33d4021fba39c474dd11fc0a2
Amar Zoranic <amar.zoranic@gmail.com>
2022-10-15 19:13:50 +0200

    dodatno

--- 11.zadatak-js/app.js
+++ 11.zadatak-js/app.js
@@ -1,26 +1,50 @@
-// function test(){
-//     console.log("iz deklarisane funkcije");
-//   }                                        // prvo se diže cela funkcija
-//   var test;                                // zatim se diže deklaracija promenjive
-//   var test = function (){
-//     console.log("iz function expression");
-//   }                                        // ostalo na istom mestu u kodu
-//   test();                                    // ostalo na istom mestu u kodu
+// // function test(){
+// //     console.log("iz deklarisane funkcije");
+// //   }                                        // prvo se diže cela funkcija
+// //   var test;                                // zatim se diže deklaracija promenjive
+// //   var test = function (){
+// //     console.log("iz function expression");
+// //   }                                        // ostalo na istom mestu u kodu
+// //   test();                                    // ostalo na istom mestu u kodu
 
 
-fetch("https://catfact.ninja/facts")
-.then((res) => {
-  res.json().then((res) => {
-    console.log(res.data);
-    max = 0;
-    for (el of res.data) {
-      if(el.length > max){
+// fetch("https://catfact.ninja/facts")
+// .then((res) => {
+//   res.json().then((res) => {
+//     console.log(res.data);
+//     max = 0;
+//     for (el of res.data) {
+//       if(el.length > max){
         
-      }
-    }
-  });
-})
-.catch((err) => {
-  console.log("nesupesno zbog erora", err);
-});
+//       }
+//     }
+//   });
+// })
+// .catch((err) => {
+//   console.log("nesupesno zbog erora", err);
+// });
 
+// ip 
+// http
+// https
+// dns
+// osi model
+// tcp 
+// ruter \
+
+
+getData = fetch('https://catfact.ninja/fact');
+
+getData
+.then((response)=> {
+    return response .json();
+})
+.then((commits)=> {
+    console.log(commits);
+    commits.data.forEach(element => {
+        console.log(element.fact);
+    });
+})
+.catch((err)=> {
+    console.log("greska", err);
+});
